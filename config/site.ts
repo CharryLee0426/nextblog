664f43ba6ee1238b4bc2fa46199d3d20c6c6cdd7
Chen <lichen000426@gmail.com>
2024-05-08 16:35:50 -0700

    Init commit

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "Chen's Blog",
+    url: "https://chenli.dev",
+    description: "Chen's Blog using NextJS",
+    author: "Chen Li",
+    links: {
+        x: "",
+        github: "",
+        personalSite: ""
+    },
+}
+
+export type SiteConfig = typeof siteConfig;
